17c9fb6b126eccf07ae2b2c06bbead861bb2d1bb
Kamil Kozioł <koziol.kamil@protonmail.com>
2024-12-08 12:00:40 +0100

    Added Vercel Analytics

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,7 +1,8 @@
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
-import "../globals.css";
 import React from "react";
+import "../globals.css";
 
 const roboto = Roboto({
   weight: ["100", "300", "400", "500", "700", "900"],
@@ -21,6 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Analytics />
       <body className={roboto.className}>{children}</body>
     </html>
   );
